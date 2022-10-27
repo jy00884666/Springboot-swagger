41f1f199e62d218c12a3f38edabf4280e027a71e
jy00884666 <87419590@qq.com>
2022-10-27 18:05:38 +0800

    20221027

--- src/main/java/com/sw/config/SwaggerAutoConfiguration.java
+++ src/main/java/com/sw/config/SwaggerAutoConfiguration.java
@@ -114,9 +114,12 @@ public class SwaggerAutoConfiguration {
         list.add(new ResponseMessageBuilder().code(400).message("请求失败,具体查看返回业务状态码与对应消息").build());
         list.add(new ResponseMessageBuilder().code(401).message("请求失败,未经过身份认证").build());
         list.add(new ResponseMessageBuilder().code(403).message("请求失败,被禁止").build());
+        list.add(new ResponseMessageBuilder().code(404).message("找不到资源").build());
         list.add(new ResponseMessageBuilder().code(405).message("请求方法不支持").build());
+        list.add(new ResponseMessageBuilder().code(409).message("业务逻辑异常").build());
         list.add(new ResponseMessageBuilder().code(415).message("请求媒体类型不支持").build());
         list.add(new ResponseMessageBuilder().code(500).message("服务器遇到了一个未曾预料的状况,导致了它无法完成对请求的处理").build());
+        list.add(new ResponseMessageBuilder().code(502).message("nginx异常").build());
         list.add(new ResponseMessageBuilder().code(503).message("服务器当前无法处理请求,这个状况是临时的，并且将在一段时间以后恢复").build());
         return list;
     }
